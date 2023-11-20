10960ddadbc29bc366728fd670a61ba8817881d8
mjuliafb <mjuliafb@gmail.com>
2023-11-20 17:01:08 +0100

    commit on vs text box fixed

--- Project BER/script.js
+++ Project BER/script.js
@@ -2,18 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const buttonToConvert = document.getElementById('button-to-convert');
   const input = document.getElementById('texto');
   const results = document.getElementById('results');
-  const contador = document.getElementById('contador');
-
-  input.addEventListener('input', function () {
-    const longitudTexto = input.value.length;
-    const maxCaracteres = 1000;
-
-    contador.textContent = `Máximo ${maxCaracteres - longitudTexto} caracteres restantes`;
-
-    if (longitudTexto > maxCaracteres) {
-      input.value = input.value.substring(0, maxCaracteres);
-    }
-  });
 
   buttonToConvert.addEventListener('click', function () {
     const texto = input.value;
